df6186317f862d0f8764e726ac99a2aa7d88a534
Aldo Bramantio Nugroho <aldobrahmantyo321@gmail.com>
2024-07-30 08:52:02 +0700

    Update minor

--- app/Http/Controllers/Api/KegiatanController.php
+++ app/Http/Controllers/Api/KegiatanController.php
@@ -53,7 +53,7 @@ class KegiatanController extends Controller
 
         $kegiatan = DB::select(DB::raw("SELECT kegiatan.sid, oss_nib as nib, kegiatan.kbli, notelp, email, pemrakarsa, judul_kegiatan, skala, kewenangan,
         to_char(to_timestamp(kegiatan.tanggal_input,'dd/MM/YYYY HH24:MI:ss'),'YYYY/MM/dd HH24:MI:ss') AS tanggal_input,
-        jenisdokumen, id_proyek, jenis_risiko, kbli, file, pkplh_doc, kl.lokasi, name_1 as prov, name_2 as kota,
+        jenisdokumen, id_proyek, jenis_risiko, kbli, file, pkplh_doc, kl.lokasi, id_izin, name_1 as prov, name_2 as kota,
         case when file is null then '-' else concat('<a class=\"btn btn-sm btn-success\" href="."https://amdal.menlhk.go.id/amdalnet', replace(file,'./assets', '/assets'), '"." target="."_blank"."><i class=\"fas fa-download\"></i></a>') end as file_url,
         case when pkplh_local_doc is null then '-' else concat('<a class=\"btn btn-sm btn-success\" href="."https://amdal.menlhk.go.id/amdalnet/assets/uploads/pkplh/', pkplh_local_doc, '"." target="."_blank"."><i class=\"fas fa-download\"></i></a>') end as pl_url
         from kegiatan
